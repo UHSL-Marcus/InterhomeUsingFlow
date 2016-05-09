e13f50ca9537d6c110a42c7edffd01937f206eef
UHSL-Marcus <m.lee23@herts.ac.uk>
2016-05-09 15:28:09 +0100

    Added author tags

---
+++ HubApp/HTTPRequest.h
@@ -0,0 +1,21 @@
+/********************************************************************
+// ************************* HTTPRequest.h **************************          
+/************************************ Author: Marcus Lee ***********/
+
+#ifndef HTTP_REQUEST_H
+#define HTTP_REQUEST_H
+
+#include <curl/curl.h>
+
+
+class HTTPRequest {
+	public:
+		HTTPRequest();
+	private:
+		
+	
+};
+
+
+
+#endif /* HTTP_REQUEST_H */
